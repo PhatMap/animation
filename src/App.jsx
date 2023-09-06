284f0144344bc793a3b852741986a4d49c07c7a3
Phat <phatmap234@gmail.com>
2023-09-06 15:46:53 +0700

    de

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Toggle from './components/Toggle'
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className='App'>
-      <Toggle />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Toggle />} />
+      </Routes>
     </div>
   );
 }
